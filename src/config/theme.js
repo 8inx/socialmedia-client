8d803db2a596b98db894699948352215850d39d8
coltx86 <acebo.dale@gmail.com>
2021-09-06 23:45:36 +0800

    Add config

---
+++ src/config/theme.js
@@ -0,0 +1,33 @@
+export const Light = {
+  name: "Light",
+  font: "Roboto, sans-serif",
+  fontweight: {
+    bold: 700,
+    medium: 500,
+    regular: 400,
+    light: 300
+  },
+  color: {
+    text:{
+      primary: "#1C1C1C",
+      secondary: "#5B5B5B"
+    },
+    body: {
+      primary: "#FFFFFF",
+      secondary: "#F0F2F5"
+    },
+    placeholder: {
+      text: "rgba( 115, 115, 115, 0.87)",
+      body: "rgba( 252, 252, 252, 1.0)"
+    },
+    button : {
+      text: "rgb(5, 5, 5)",
+      body: "rgb(228, 230, 235)"
+    },
+    scrollbar: {
+      handle:"rgba( 34, 36, 37, 0.15)",
+    },
+    border: "rgba( 34, 36, 37, 0.15)",
+    boxShadow: "0 3px 10px rgba(0, 0, 0, 0.2)",
+  }
+}
