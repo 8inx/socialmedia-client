2265de6c7c94127015844d3a3f1bc50daf1afb68
coltx86 <acebo.dale@gmail.com>
2021-09-07 17:26:50 +0800

    Update AppThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom"
-
+import { Light } from "config/theme";
+import AppThemeProvider from "components/AppThemeProvider/AppThemeProvider";
 
 const AppRoutes = () => {
   return(
@@ -12,9 +13,11 @@ const AppRoutes = () => {
     </Router>
   )
 }
+//unsplash.it/200/200
 
-
-const App = () => 
+const App = () =>
+  <AppThemeProvider theme={Light}>
     <AppRoutes/> 
+  </AppThemeProvider>
 
 export default App;
